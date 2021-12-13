f3fbe73bf96ee8bf2d011a773d857a6c8be9ffb1
Reht Cavazos <reht.dev@gmail.com>
2021-12-13 16:22:13 -0600

    display movie title results from search, removed zip input to be added to page 2, added listener to display only targed data. Need to create a button 'search local showtimes' to redirect to 2nd html/js page and launch the search function from there.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var date = document.querySelector('#date');
+var movieTitleDisplay = document.querySelector('.movie-title-display')
 date.textContent = moment().format('dddd Do MMMM, YYYY');
 
 
 //Defining the intial value
 const API_KEY = "27eb4a424f68db4c8bc0fea4d921efa7";
-const url =
-    "https://api.themoviedb.org/3/search/movie?api_key=27eb4a424f68db4c8bc0fea4d921efa7";
+// const API_KEY_OMBD = '6ece1090'
+const url = "https://api.themoviedb.org/3/search/movie?api_key=27eb4a424f68db4c8bc0fea4d921efa7";
 
 //Selecting Elements for the
 const searchBtn = document.querySelector("#search");
@@ -47,11 +48,36 @@ searchBtn.onclick = function (event) {
 
     console.log("value: ", value);
     console.log("hello this button has been clicked");
-
+//search for movie entered into search and return the data
     fetch(newUrl).then(function (res) {
         if (res.ok) {
             res.json().then(function (data) {
                 console.log(data);
+                //clear data display window of any previous searches
+                movieTitleDisplay.textContent = ''
+                //loop through the data results and create each title as a p
+                for (var i = 0; i < data.results.length; i++) {
+                    var movieDiv = document.createElement('div')
+                    movieTitleDisplay.append(movieDiv)
+                    var movieTitle = document.createElement('p');
+                    movieTitle.setAttribute('id', data.results[i].id)
+                    movieTitle.textContent = data.results[i].title;
+                    movieDiv.append(movieTitle)
+                    //on target click clear the results and display info on the movie selected, showing the tile as h1 and info as p
+                    movieTitle.addEventListener('click', function (e) {
+                        movieTitleDisplay.textContent = ''
+                        console.log(data.results);
+                        var filtered = data.results.filter(item => item.id == e.target.id)
+                        console.log(filtered);
+                        var title= document.createElement('h1')
+                        title.textContent = filtered[0].title
+                        var info = document.createElement('p')
+                        info.textContent = filtered[0].overview
+                        
+                        // create btn for showtime search and append to mtd 
+                        movieTitleDisplay.append(title, info)
+                    })
+                }
             });
         } else {
             //change to modal
@@ -61,10 +87,13 @@ searchBtn.onclick = function (event) {
     console.log("Value ", value);
 };
 
+function getZip(movieTitle) {
+    console.log(movieTitle);
+}
 
 //clear history button function
 clearBtn.onclick = function clearSearches() {
-            //change to a modal
+    //change to a modal
     if (confirm("Are you sure you want to clear all searches?")) {
         window.localStorage.clear();
     } else { }
