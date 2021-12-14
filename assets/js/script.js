dca7c13c0bd613d2a8125e0cb8a09b1c228cd708
Reht Cavazos <reht.dev@gmail.com>
2021-12-14 13:31:24 -0600

    applied a hidden function to the results placeholders in main div

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ const url = "https://api.themoviedb.org/3/search/movie?api_key=27eb4a424f68db4c8
 const searchBtn = document.querySelector("#search");
 const clearBtn = document.querySelector("#clear");
 const inputElement = document.querySelector("#inputValue");
-// const movieSearchable = document.querySelector("#movies-searchable");
 
 //movie poster to populate
 function movieSection(movies) {
@@ -44,36 +43,44 @@ function createMovieContainer(movies) {
 //executes search, clears input value
 searchBtn.onclick = function (event) {
     event.preventDefault();
+    //takes the input from the searchbar and applies it to the api url
     const value = inputElement.value;
     const newUrl = url + "&query=" + value;
     inputElement.value = '';
 
-    console.log("value: ", value);
-    console.log("hello this button has been clicked");
-//search for movie entered into search and return the data
+    //hides the populate-here placeholders on click. need to change it to only hide on successful search execution not on empty clicks or no returns
+    $('#populate-here').hide();
+    $('.placeholder').hide();
+
+    //search for movie entered into search and return the data
     fetch(newUrl).then(function (res) {
         if (res.ok) {
             res.json().then(function (data) {
+
                 console.log(data);
+
                 //clear data display window of any previous searches
                 movieTitleDisplay.textContent = ''
+
                 //loop through the data results and create each title as a p
                 for (var i = 0; i < data.results.length; i++) {
                     var movieDiv = document.createElement('div')
                     movieTitleDisplay.append(movieDiv)
                     var movieTitle = document.createElement('p');
+                    //set class, id of each p
                     movieTitle.setAttribute('class', 'box')
-
                     movieTitle.setAttribute('id', data.results[i].id)
+                    //add the text content from the returned data titles and lsit them
                     movieTitle.textContent = data.results[i].title;
                     movieDiv.append(movieTitle)
+
                     //on target click clear the results and display info on the movie selected, showing the tile as h1 and info as p
                     movieTitle.addEventListener('click', function (e) {
                         movieTitleDisplay.textContent = ''
                         console.log(data.results);
                         var filtered = data.results.filter(item => item.id == e.target.id)
                         console.log(filtered);
-                        var title= document.createElement('h1')
+                        var title = document.createElement('h1')
                         title.textContent = filtered[0].title
                         var info = document.createElement('p')
                         info.textContent = filtered[0].overview
@@ -90,16 +97,11 @@ searchBtn.onclick = function (event) {
             alert("error:" + res.statusText);
         }
     });
-    console.log("Value ", value);
 };
 
 //this links the second html created for the local showtimes page
 function toLocalSearchPage() {
     location.href = "local-search.html";
-}
-
-function getZip(movieTitle) {
-    console.log(movieTitle);
 }
 
 //clear history button function
