bc6378ab9fc6a1bce18583801af81fd71574b092
alfonsof4 <alfonsofernandez4th@gmail.com>
2021-12-15 13:00:29 -0600

    return data from movie API

--- assets/js/local-search.js
+++ assets/js/local-search.js
@@ -2,54 +2,46 @@
 
 // after user inputs zip in search bar dynamically display local showtimes in the results div
 
-// declare variables
+// declare variable and get url
 var apiKey = "xfkuutw67xzmu7cs8dk27w3j";
 var baseUrl = "http://data.tmsapi.com/v1.1";
-var showtimesUrl = baseUrl + 'movies/showings';
-var zipCode = "78701";
+var showtimesUrl = baseUrl + '/movies/showings';
+var zip;
 var d = new Date();
 var today = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
 
-$(document).ready(function() {
+function movieSearch(e) {
+    e.preventDefault()
+    zip = $('#inputValue').val().trim()
 
     //send off the query
     $.ajax({
         url: showtimesUrl,
-            data: {
-                zip: zipCode,
+            data: { startDate: today,
+                zip: zip,
                 jsonp: "dataHandler",
                 api_key: apiKey
             },
         dataType: "jsonp",
     });
-});
-
-//callback function
-
-// zip code api call using fetch
-// function zipSearch() {
-//     fetch(baseUrl).then(function (res) {
-//       if (res.ok) {
-//         res.json().then(function (data) {
-//           console.log("test", data);
-//           const areaCode = data.results;
-//           const movieBlock = createMovieContainer(movies);
-//           movieSearchable.innerHTML = movieBlock;
-//         });
-//       } else {
-//         alert("error:" + res.statusText);
-//       }
-//     });
-//   }
-
+};
 
 function dataHandler(data) {
-$(document.body).append('<p>Found' + data.length + ' movies showing within 5 miles of ' + zipCode+':</p>');
+$(document.body).append('<p>Found' + data.length + ' movies showing within 5 miles of ' + zip+':</p>');
+    console.log("data:",data)
     var movies = data.hits;
-    $.each(data, function(index, movie) {
-        var movieData = '<div class="tile"><img src="http://developer.tmsimg.com/' + movie.preferredImage.uri + '?api_key='+apiKey+'"><br/>';
+    $.each(data, function(index, movie) {  // this is a jquery for each loop
+        var movieData = `<div class="tile"><br/>`;
         movieData += movie.title;
         if (movie.ratings) { movieData += ' (' + movie.ratings[0].code + ') </div>' };
         $(document.body).append(movieData);
     });
 }
+
+// getapi(apiUrl)
+$('#search').click(movieSearch) // jquery version of below
+
+// In the DOM you have the search form
+// Line 74 is a click listenter
+// when the user clicks the button it runs the movieSearch function
+// movieSearch gets the zip from the input field and runs the search
